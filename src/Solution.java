a4c476f6d5193cb20a23666f7445f815d34f3767
Sanjeev Mallick <sanjeevmallick2@gmail.com>
2022-12-01 22:17:22 +0530

    Testing input format

--- src/Solution.java
+++ src/Solution.java
@@ -8,25 +8,40 @@ public class Solution {
 
         //Testing purpose of classes and methods-->
 
-       /* Scanner sc=new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter station number:");
+//        String stationNumber = sc.nextLine();
+//        System.out.println("Enter station name:");
+//        String stationName = sc.nextLine();
+//        System.out.println("Enter compulsory subjects:");
+//     Arrays.asList(Subjects.values()).forEach(System.out::println);
+//
+//        System.out.println("Enter Branch Criterion for the Station:");
+//        String branch = sc.nextLine();
+//        ArrayList<Branches> branchAllowed = new ArrayList<>();
+//        Arrays.asList(branch.strip().split(","))
+//                .forEach((String sub) -> {
+//                    branchAllowed.add(Branches.valueOf(sub.strip()));
+//                });
+//        System.out.println(branchAllowed);
+//        System.out.println("Enter student name:");
+//        String studentName = sc.nextLine();
+//        System.out.println("Enter student CG:");
+//        double studentCG = sc.nextDouble();
+//        System.out.println("Enter student ID:");
+//        String studentID = sc.next();
+//        System.out.println("Enter student Branch:");
+//        String studentBranch = sc.next();
+//        System.out.println("Enter student's completed subjects:");
+//        String subjects = sc.nextLine();
+//        ArrayList<Subjects> subjectsCompleted = new ArrayList<>();
+//        Arrays.asList(subjects.strip().split(","))
+//                .forEach((String sub) -> {
+//                    subjectsCompleted.add(Subjects.valueOf(sub.strip()));
+//                });
+//        System.out.println(subjectsCompleted);
 
-        System.out.println("Enter station number:");
-        String stationName=sc.nextLine();
-        System.out.println("Enter station name:");
-        String stationNumber=sc.nextLine();
-        System.out.println("Enter compulsory subjects:");
-        Arrays.asList(Subjects.values()).forEach(System.out::println);
-        System.out.println("Enter station's Branches allowed:");
-
-        System.out.println("Enter student name:");
-        String studentName=sc.nextLine();
-        System.out.println("Enter student CG:");
-        double studentCG=sc.nextInt();
-        System.out.println("Enter student ID:");
-        String studentID=sc.nextLine();
-        System.out.println("Enter student Branch:");
-        String studentBranch=sc.nextLine();
-        System.out.println("Enter student's completed subjects:"); */
 
 //
 //        Admin.stations.add(new Station("Station1", "location1", 9, Arrays.stream(new String[]{"CS", "ECE", "EEE"}).toList()));
@@ -60,27 +75,27 @@ public class Solution {
 
 
 /*********************************************
-        Admin.stations.add(new Station("Station1", "location1", 9, Arrays.stream(new String[]{"CS", "ECE", "EEE"}).toList()));
-        Admin.stations.add(new Station("Station2", "location2", 8, Arrays.stream(new String[]{"Mech", "ECE", "EEE"}).toList()));
-        Admin.stations.add(new Station("Station3", "location3", 7, Arrays.stream(new String[]{"Civil", "ECE", "EEE"}).toList()));
-        Admin.stations.add(new Station("Station4", "location4", 6, Arrays.stream(new String[]{"Chemical", "ECE", "EEE"}).toList()));
+ Admin.stations.add(new Station("Station1", "location1", 9, Arrays.stream(new String[]{"CS", "ECE", "EEE"}).toList()));
+ Admin.stations.add(new Station("Station2", "location2", 8, Arrays.stream(new String[]{"Mech", "ECE", "EEE"}).toList()));
+ Admin.stations.add(new Station("Station3", "location3", 7, Arrays.stream(new String[]{"Civil", "ECE", "EEE"}).toList()));
+ Admin.stations.add(new Station("Station4", "location4", 6, Arrays.stream(new String[]{"Chemical", "ECE", "EEE"}).toList()));
 
-        Student s1 = new Student("name1", "BITSEmailId1", "id", "ECE", 9.0, "password");
-        Student s2 = new Student("name2", "BITSEmailId2", "id", "ECE", 7.5, "password");
-        Student s3 = new Student("name3", "BITSEmailId3", "id", "Branch", 6.0, "password");
+ Student s1 = new Student("name1", "BITSEmailId1", "id", "ECE", 9.0, "password");
+ Student s2 = new Student("name2", "BITSEmailId2", "id", "ECE", 7.5, "password");
+ Student s3 = new Student("name3", "BITSEmailId3", "id", "Branch", 6.0, "password");
 
-        ArrayList<Station> preferenceList=new ArrayList<>();
+ ArrayList<Station> preferenceList=new ArrayList<>();
 
 
-        Admin.students.add(s1);
-        Admin.students.add(s2);
+ Admin.students.add(s1);
+ Admin.students.add(s2);
 
-        s2.addPreference("Station1");
-        s2.addPreference("Station2");
-        s2.addPreference("Station3");
+ s2.addPreference("Station1");
+ s2.addPreference("Station2");
+ s2.addPreference("Station3");
 
-        Admin.allocate(s2);
-*************************************************/
+ Admin.allocate(s2);
+ *************************************************/
 
 //        Station st1=new Station("bitsp","pilani",8.5,Arrays.stream(new String[]{"Chemical", "ECE", "EEE"}).toList());
 //        Station st2=new Station("bitsg","goa",8.0,Arrays.stream(new String[]{"Chemical", "ECE", "EEE"}).toList());
@@ -115,9 +130,6 @@ public class Solution {
 //        }
 
 
-
-
-
 //        System.out.println(s3.getBITSEmailId());
 //        s2.SubjectsCompleted.add(Subjects.OOP);
 //        System.out.println(s2.getSubjectsCompleted());
@@ -137,7 +149,6 @@ public class Solution {
 //
 //
 //        Collections.sort(StudentArray);
-
 
 
     }
